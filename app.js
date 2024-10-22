075caba850b656d327f3c422d19cad8d57b72544
devazteca <developerdep1@gmail.com>
2024-10-21 18:55:11 -0600

    Prueba v3.4.2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000; //TODO: Usa el puerto proporcionado por e
 
 //TODO: Configura CORS para permitir solicitudes desde localhost:3000
 app.use(cors({
-    origin: 'https://invernadero-alpha.vercel.app/?vercelToolbarCode=c7uMqfVHvSfOAjm', //TODO: Permite el acceso desde el frontend
+    origin: 'http://localhost:3000', //TODO: Permite el acceso desde el frontend
     methods: ['GET', 'POST'], //TODO: Métodos permitidos
     credentials: true, //TODO: Si necesitas usar cookies o sesiones
 }));
